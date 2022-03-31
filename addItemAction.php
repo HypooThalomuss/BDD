1e2f06bb381c0f69b96a262f65c80341335f29de
JacobD1998 <98901966+JacobD1998@users.noreply.github.com>
2022-03-31 18:27:46 -0400

    Update addItemAction.php

--- addItemAction.php
+++ addItemAction.php
@@ -1,23 +1,33 @@
 <?php
 
-    include 'DBConnect.php';
+    //Get form data
+    $itemName = $_GET["itemName"];
+    $itemLocation = $_GET["itemLocation"];
+    $itemAmount = $_GET["itemAmount"];
+    $itemPrice = $_GET["itemPrice"];
+    
+    //Connect to the database
+    $servername = "localhost";
+    $username = "Mahadev";
+    $password = "mahadev";
+    $dbname = "bdd_inventory_management";
 
-    // prepare and bind
-    try {
-        $sql = "INSERT INTO products (Name, Location, Amount, Price,) VALUES (?, ?, ?, ?)";
-        $conn = openConn();
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param($itemName, $itemLocation, $itemAmount, $itemPrice);
-
-        $itemName = $_GET["itemName"];
-        $itemLocation = $_GET["itemLocation"];
-        $itemAmount = $_GET["itemAmount"];
-        $itemPrice = $_GET["itemPrice"];
-
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
-    } catch (Exception $e) {
-        echo 'Exception Message: ' . $e->getMessage();
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    
+    //Create SQL Statement
+    $sql = "INSERT INTO products (ProductID, Name, Location, Amount, Price) VALUES ('0', '$itemName', '$itemLocation', '$itemAmount', '$itemPrice')";
+    
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New item added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 ?>
