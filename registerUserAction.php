ebabd51202c4a262fe14903d5ca27acc0d16ea54
JacobD1998 <98901966+JacobD1998@users.noreply.github.com>
2022-03-31 18:26:35 -0400

    Update registerUserAction.php

--- registerUserAction.php
+++ registerUserAction.php
@@ -1,32 +1,38 @@
 <?php
 
-    include 'DBConnect.php';
-
-    // prepare and bind
-    try {
-        $sql = "INSERT INTO users (FirstName, LastName, Username, Password, Type, Question, Answer) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $conn = openConn();
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param($first, $last, $user, $passWord, $type, $question, $answer);
-
-        //Get form data
-        $first = $_POST["firstName"];
-        $last = $_POST["lastName"];
-        $user = $_POST["username"];
-        $passWord = $_POST["password"];
-        $passConfirm = $_POST["passwordConfirm"];
-        $type = $_POST["usertype"];
-        $question = $_POST["securityQuestion"];
-        $answer = $_POST["securityAnswer"];
     
-        if($passWord != $passConfirm){
-            die("Password and Confirm Password fields do not match!");
-         }
+    //Get form data
+    $first = $_GET["firstName"];
+    $last = $_GET["lastName"];
+    $user = $_GET["username"];
+    $passWord = $_GET["password"];
+    $passConfirm = $_GET["passwordConfirm"];
+    $type = $_GET["usertype"];
+    $question = $_GET["securityQuestion"];
+    $answer = $_GET["securityAnswer"];
+    
+    //Connect to the database
+    $servername = "localhost";
+    $username = "Mahadev";
+    $password = "mahadev";
+    $dbname = "bdd_inventory_management";
 
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
-    } catch (Exception $e) {
-        echo 'Exception Message: ' . $e->getMessage();
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    
+    //Create SQL Statement
+    $sql = "INSERT INTO users (UserID, FirstName, LastName, Username, Password, Type, Question, Answer) VALUES ('0', '$first', '$last', '$user', '$passWord', '$type', '$question', '$answer')";
+    
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New user added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 ?>
