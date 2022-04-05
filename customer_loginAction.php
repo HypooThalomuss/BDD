a093a0b4e2adaf9464250823d31c3b5c42768857
HypooThalomuss <95379541+HypooThalomuss@users.noreply.github.com>
2022-04-05 19:38:29 -0400

    Update customer_loginAction.php

--- customer_loginAction.php
+++ customer_loginAction.php
@@ -1,41 +1,38 @@
 <?php
 
-//get the form data
-$userkey = $_POST["uname"];
-$passkey = $_POST["pass"];
+    
+    //Get form data
+    $first = $_GET["firstName"];
+    $last = $_GET["lastName"];
+    $user = $_GET["username"];
+    $passWord = $_GET["password"];
+    $passConfirm = $_GET["passwordConfirm"];
+    $type = $_GET["usertype"];
+    $question = $_GET["securityQuestion"];
+    $answer = $_GET["securityAnswer"];
+    
+    //Connect to the database
+    $servername = "localhost";
+    $username = "mahadev";
+    $password = "mahadev";
+    $dbname = "bdd_inventory_management";
 
-//connect to the database
-$servername = "localhost";
-$username = "mahadev";
-$password = "mahadev";
-$dbname = "bdd inventory management";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    //Create SQL Statement
+    $sql = "INSERT INTO users (UserID, FirstName, LastName, Username, Password, Type, Question, Answer) VALUES ('0', '$first', '$last', '$user', '$passWord', '$type', '$question', '$answer')";
+    
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New user added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-echo "$userkey $passkey ";
-//generate an SQL statement and send it to the database
-$sql = "SELECT Username FROM users WHERE Username = '$userkey' AND Password = '$passkey'";
-$result = $conn->query($sql);
-$type = gettype($result);
-echo "$type ";
-
-//check if credentials are correct and link to appropriate page
-if ($result->num_rows > 0) {
-  // username and password are correct
-  echo "at least 1 result ";
-  header("Location: employee_function_select.html");
- }
- else {
-  // username and/or password are incorrect
-   echo "0 results ";
-   header("Location: login_customer.html");
-}
-
-
-$conn->close();
+    $conn->close();
 ?>
