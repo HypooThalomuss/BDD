0ed782517b7b57c2d511bd330f255bb9384add35
HypooThalomuss <95379541+HypooThalomuss@users.noreply.github.com>
2022-04-11 18:50:39 -0400

    Update customer_loginAction.php

--- customer_loginAction.php
+++ customer_loginAction.php
@@ -1,8 +1,8 @@
 <?php
 
 //get the form data
-$userkey = $_GET["uname"];
-$passkey = $_GET["pass"];
+$userkey = $_GET["username"];
+$passkey = $_GET["password"];
 
 //connect to the database
 $servername = "localhost";
@@ -20,21 +20,21 @@ if ($conn->connect_error) {
 echo "$userkey $passkey ";
 //generate an SQL statement and send it to the database
 $sql = "SELECT Username FROM users WHERE Username = '$userkey' AND Password = '$passkey'";
-$result = mysqli_query($conn,$sql);
+$result = $conn->query($sql);
 $count = mysqli_num_rows($result);
-//$result = $conn->query($sql);
-//$type = gettype($result);
+$type = gettype($result);
 echo "$type ";
 
 //check if credentials are correct and link to appropriate page
 if ($count > 0) {
+
   // username and password are correct
-  echo "at least 1 result ";
+  //echo "Login Sucessful";
   header("Location: employee_product_list.html");
  }
  else {
   // username and/or password are incorrect
-   echo "0 results ";
+   //echo "Login failed";
    header("Location: login_customer.html");
 }
 
