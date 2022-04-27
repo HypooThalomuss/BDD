63b139f86ce0df52da42de058a8494d617a61230
TPButlerV <99680375+TPButlerV@users.noreply.github.com>
2022-04-27 10:16:09 -0400

    Update employee_product_list.php

--- employee_product_list.php
+++ employee_product_list.php
@@ -1,16 +1,64 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<html lang = "en">
+	<head>
+		<!--page title-->
+		<title>Product List</title>
+		<!--css-->
+		<link rel="stylesheet" type="text/css" charset="utf-8" href="inventory_fp_css.css">
+		<!--Google Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+	</head>
+	<body>
+		<div class = "block">
+		
+		<h1>Product List</h1>
+		
+		<p>
+			<a href="employee_function_select.html">Back to employee home page</a>
+		</p>
+		<hr>
+		
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th>In Stock</th>
+                        <th>Price</th>
+                        <th></th>
+                    </tr>
+                    <?php
+                        include 'DBConnect.php';
+                        
+                        $sql = "SELECT * FROM products";
+                        $result = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>"
+                            . "<td>".$row['Name']."</td>"
+                            . "<td>".$row['Location']."</td>"
+                            . "<td>".$row['Amount']."</td>"
+                            . "<td>".$row['Price']."</td>"
+                            . "<td><form action='changeQuantity.php'>"
+                            . "<input type='text' name='productID' value=".$row['ProductID']." readonly hidden>"
+                            . "<input type='submit' value='Change Quantity' class='buttontype2'>"
+                            ."</td>"
+                            . "</tr>";
+                            }
+                        }
+                    ?>
+                </table>
+		
+		<p>
+			<a href="employee_function_select.html">Back to employee home page</a>
+		</p>
+		</div>
+		
+		<!--code taken from https://www.geeksforgeeks.org/html-window-alert-method/ -->
+		<script>
+			function changeQuantity() {
+            			window.open("quantity_form.html", "change quantity", "width=400,height=125");
+        		}
+		</script>
+	</body>
 </html>
+
